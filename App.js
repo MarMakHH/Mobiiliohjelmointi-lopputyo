25fc22c3da72d2d1c6dd73135e598164d8ff34d6
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2025-04-10 22:09:26 +0300

    Added tab navigations

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import LearnScreen from './components/LearnScreen';
+import CreateScreen from './components/CreateScreen';
+import SettingsScreen from './components/SettingsScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({  // Navigator can be customized using screenOptions
+          tabBarIcon: ({ focused, color, size }) => { 
+            // Function tabBarIcon is given the focused state,
+	    // color and size params
+            let iconName;
+
+            if (route.name === 'Learn') {
+              iconName = 'book';
+            } else if (route.name === 'Create') {
+              iconName = 'create';
+            } else if (route.name === 'Settings') {
+              iconName = 'settings'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;   //it returns an icon component
+          },
+        })}>
+        <Tab.Screen name='Learn' component={LearnScreen} />
+        <Tab.Screen name='Create' component={CreateScreen} />
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
